fb3e0aae0af95075bc8fff375be2b1e5b6cd340a
Eder Manjarres <41306844+emantho@users.noreply.github.com>
2024-05-22 19:25:48 +0000

    cambios en nombres de rutas

--- Alba/pasticceria/urls.py
+++ Alba/pasticceria/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('client_list', views.client_list, name='client_list'),
+    path('abm_clients', views.abm_clients, name='abm_clients'),
 ]
--- Alba/pasticceria/views.py
+++ Alba/pasticceria/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from . import forms
 import datetime
 
 # Create your views here.
 def index(request):
     my_context_var = {
-        'userName':'Diana',
+        'userName':'Paola',
         'dateTime': datetime.datetime.now()
     }
     
     return render(request, 'pasticceria/index.html', my_context_var)
 
+def client_list(request):
+    
+    my_context = {
+        'names' : [
+            "Carlos Perez",
+            "Patricia Fernandez",
+            "Emanuel Manrique"
+        ], 
+        'payment_status': True
+        
+    }
+    
+    return render(request, 'pasticceria/client_list.html', my_context)
+
+def abm_clients(request):
+    
+    # Controlling request flow 
+    
+    context = {}
+    
+    if request == 'GET':
+        context['abm_users_form'] = forms.AbmClientsForm()
+    
+    else: # Asummig is a POST
+        context['abm_users_form'] = forms.AbmClientsForm(request.POST)
+    
+        # Form validation
+        # If correct, inform with message and redirect 
+
+        # IF NO correct 
+        # Saty in form but showing an error
+        # return redirect('index')
+        
+    return render(request, 'pasticceria/abm_clients.html', context)
