a4e139832fa4bb4ec79448b77a249960a4ec8b09
Eder Manjarres <41306844+emantho@users.noreply.github.com>
2024-05-23 20:50:03 +0000

    + applied postgresql database information + created requirements.txt to quick python isntall

--- Alba/Alba/settings.py
+++ Alba/Alba/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "Alba.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "alba_pasticceria",
+        "USER":"postgres",
+        "PASSWORD":"pass123",
+        "HOST":"172.17.0.2",
+        "PORT":"5432",
     }
 }
 
