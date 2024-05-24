4180b3a2f412b6e1d6e7e983680bc8a89bd2c7be
Eder Manjarres <emanja1014@gmail.com>
2024-05-23 21:41:14 -0300

    + creadas views admin, cafe, menu, carrito y sus correspondientes css + añadidas urls

--- Alba/pasticceria/urls.py
+++ Alba/pasticceria/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('client_list', views.client_list, name='client_list'),
     path('abm_clients', views.abm_clients, name='abm_clients'),
+    path('menu', views.menu, name='menu'),
+    path('cafe', views.cafe, name='cafe'),
+    path('carrito', views.carrito, name='carrito'),
+    path('admin', views.admin, name='admin'),
 ]
--- Alba/pasticceria/views.py
+++ Alba/pasticceria/views.py
@@ -5,50 +5,76 @@ from . import forms
 import datetime
 from django.contrib import messages
 
+
 # Create your views here.
 def index(request):
     my_context_var = {
-        'userName':'Visitante',
+        "userName": "Visitante",
         # 'dateTime': datetime.datetime.now()
     }
-    
-    return render(request, 'pasticceria/index.html', my_context_var)
+
+    return render(request, "pasticceria/index.html", my_context_var)
+
 
 def client_list(request):
-    
+
     my_context = {
-        'names' : [
-            "Carlos Perez",
-            "Patricia Fernandez",
-            "Emanuel Manrique"
-        ], 
-        'payment_status': True
-        
+        "names": ["Carlos Perez", "Patricia Fernandez", "Emanuel Manrique"],
+        "payment_status": True,
     }
-    
-    return render(request, 'pasticceria/client_list.html', my_context)
+
+    return render(request, "pasticceria/client_list.html", my_context)
+
 
 def abm_clients(request):
-    
-    # Controlling request flow 
-    
+
+    # Controlling request flow
+
     context = {}
-    
+
     # Empty form request
-    if request == 'GET':
-        context['abm_clients_form'] = forms.AbmClientsForm()
-    
-    else: # Asummig is a POST
+    if request == "GET":
+        context["abm_clients_form"] = forms.AbmClientsForm()
+
+    else:  # Asummig is a POST
         form = forms.AbmClientsForm(request.POST)
-        context['abm_clients_form'] = form
-    
+        context["abm_clients_form"] = form
+
         # Form validation
         if form.is_valid():
-        # If correct, inform with message and redirect
-            messages.success(request, 'Client created successfully')
-            return redirect('index')
-            
-        # IF NO correct 
+            # If correct, inform with message and redirect
+            messages.success(request, "Client created successfully")
+            return redirect("index")
+
+        # IF NO correct
         # Saty in form but showing an error
-        
-    return render(request, 'pasticceria/abm_clients.html', context)
+
+    return render(request, "pasticceria/abm_clients.html", context)
+
+def menu(request):
+    my_context = {
+        "mensaje": "Vista para menu"
+    }
+
+    return render(request, "pasticceria/menu.html", my_context)
+
+def cafe(request):
+    my_context = {
+        "mensaje": "Vista para cafe"
+    }
+
+    return render(request, "pasticceria/cafe.html", my_context)
+
+def carrito(request):
+    my_context = {
+        "mensaje": "Vista para carrito"
+    }
+
+    return render(request, "pasticceria/carrito.html", my_context)
+
+def admin(request):
+    my_context = {
+        "mensaje": "Vista para admin"
+    }
+
+    return render(request, "pasticceria/admin.html", my_context)
