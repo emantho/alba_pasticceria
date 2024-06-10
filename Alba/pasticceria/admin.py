93be9c767c4f02647fbc48b0c0c7ee522726ad42
Eder Manjarres <emanja1014@gmail.com>
2024-06-10 18:46:55 -0300

    Activado Djando Admin + Creado super user + Añadidos los sites

--- Alba/pasticceria/admin.py
+++ Alba/pasticceria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente, Vendedor, Producto, Orden
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Vendedor)
+admin.site.register(Producto)
+admin.site.register(Orden)
