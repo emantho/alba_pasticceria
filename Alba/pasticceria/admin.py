5ba44303acc2e894e9ba65513048b250acb48ac6
Eder Manjarres <emanja1014@gmail.com>
2024-06-14 08:52:06 -0300

    Añadido ordenItes to Admin.py

--- Alba/pasticceria/admin.py
+++ Alba/pasticceria/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cliente, Vendedor, Producto, Orden
+from .models import Cliente, Vendedor, Producto, Orden, OrdenItem
 
 # Register your models here.
 admin.site.register(Cliente)
 admin.site.register(Vendedor)
 admin.site.register(Producto)
 admin.site.register(Orden)
+admin.site.register(OrdenItem)
