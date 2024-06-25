49e2f3b491e0df9949ceb08d070d3a71a1f8fe55
Eder Manjarres <emanja1014@gmail.com>
2024-06-24 23:10:18 -0300

    cambio en el serializer de Cliente para mostrar todos los campos

--- Alba/api/serializers.py
+++ Alba/api/serializers.py
@@ -4,7 +4,7 @@ from pasticceria.models import Cliente, Producto, Vendedor, Orden, OrdenItem
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['dni', 'nombre', 'apellido', 'email']
+        fields = ['id','dni','nombre','apellido','email','direccion','ciudad','telefono','cumpleaños']
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
